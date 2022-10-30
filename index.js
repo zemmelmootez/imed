b47d96da42b29bbd74ba6df423998f7919543ea3
zemmelmootez <zemmel.mootez@gmail.com>
2022-10-30 14:38:00 +0100

    ya imed badelt css

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//hello
+arr = [1,2,5]
+
+arr.pop()
+ console.log(arr)
+
+ persone={
+     nom:"hello",
+     age:15,
+      walk()
+     {
+        return "hello"
+     }
+
+ }
+a="5"
+ console.log(typeof(a))
